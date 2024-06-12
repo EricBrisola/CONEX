462310d92ecb21c568bb0b42af99037e45563111
EricBrisola <eric.coca13@gmail.com>
2024-06-11 23:31:51 -0300

    style: added custom tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'custom': '1px 6px 4.5px 0 rgba(0, 0, 0, 0.25)',
+      },
+      fontFamily: {
+        'JosefinSans': ['Josefin Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }

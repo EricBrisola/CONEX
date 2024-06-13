5202a52f711412df68f941607c479bafd177151a
EricBrisola <eric.coca13@gmail.com>
2024-06-13 19:18:34 -0300

    style: changed font weight

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const navLinksStyle =
-    "font-JosefinSans rounded-2xl px-2 py-1 text-2xl font-bold hover:bg-[#B6DCF7] hover:text-white duration-200";
+    "font-JosefinSans rounded-2xl px-2 py-1 text-2xl hover:bg-[#B6DCF7] hover:text-white duration-200";
   return (
-    <nav className="shadow-custom flex h-[6.688rem] w-[55.813rem] items-center justify-center rounded-[2.188rem] bg-white">
-      <div className="shadow-custom bg-custom flex h-[3.375rem] w-[42.938rem] items-center justify-center gap-[0.625rem] rounded-[2.125rem]">
+    <nav className="flex h-[6.688rem] w-[55.813rem] items-center justify-center rounded-[2.188rem] bg-white shadow-custom">
+      <div className="bg-custom flex h-[3.375rem] w-[42.938rem] items-center justify-center gap-[0.625rem] rounded-[2.125rem] shadow-custom">
         <p className={navLinksStyle}>
           <Link to={"/"}>Início</Link>
         </p>

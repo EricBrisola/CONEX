1ff675efe9669fe5ef9ab74d41a24f9a44e1e612
Vini <vinirnadolny@gmail.com>
2024-06-13 18:37:27 -0300

    footer container

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,20 @@
+import cursorImage from '../../assets/homePage/divConexe/cursormouse-1.png'
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='w-screen h-[470px] mt-[110px]'>
+      <div className='w-full h-2/5 flex flex-col items-center justify-start'>
+        <div className='bg-[#b6dcf7] w-full h-full rounded-t-full flex justify-start items-center flex-col overflow-visible'>
+          <div className='flex items-center justify-center h-[210px] w-2/5 bg-[#b6dcf7] rounded-full shadow-[-50px_35px_5px_-15px_rgba(0,0,0,0.3)] relative top-[-100px]'>
+            <h1 className='font'>Conexe Conosco</h1>
+          </div>
+        </div>
+      </div>
+      
+      <div className='bg-[#b6dcf7] h-3/5'>
+
+      </div>
+    </div>
+  );
 };
 export default Footer;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 const Home = () => {
   return (
-    <main className="h-screen w-screen bg-[#EDF0EB]">
+    <main className="h-screen w-screen bg-[#EDF0EB] overflow-x-hidden">
       <Header />
+      <Footer />
     </main>
   );
 };

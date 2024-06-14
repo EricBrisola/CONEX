9425525bd709e7ce0f428b832f13032acfd81a37
EricBrisola <eric.coca13@gmail.com>
2024-06-14 01:17:10 -0300

    refactor: changed from px to rem

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,22 +1,23 @@
-import cursorImage from '../../assets/homePage/divConexe/cursormouse-1.png'
+import cursorImage from "../../assets/homePage/divConexe/cursormouse-1.png";
 
 const Footer = () => {
   return (
-    <div className='w-screen h-[470px] mt-[110px]'>
-      <div className='w-full h-2/5 flex flex-col items-center justify-start'>
-        <div className='bg-[#b6dcf7] w-full h-full rounded-t-full flex justify-start items-center flex-col overflow-visible'>
-          <div className='flex items-center justify-center h-[210px] w-2/5 bg-[#b6dcf7] rounded-full shadow-[-50px_35px_5px_-15px_rgba(0,0,0,0.3)] relative top-[-100px]'>
-            <h1 className='font-medium text-[54px]'>Conexe Conosco</h1>
+    <div className="mt-[6.875rem] h-[29.375rem] w-screen">
+      <div className="flex h-2/5 w-full flex-col items-center justify-start">
+        <div className="flex h-full w-full flex-col items-center justify-start overflow-visible rounded-t-full bg-[#b6dcf7]">
+          <div className="relative top-[-6.25rem] flex h-[13.125rem] w-2/5 items-center justify-center rounded-full bg-[#b6dcf7] shadow-[-3.125rem_2.1875rem_0.3125rem_-0.9375rem_rgba(0,0,0,0.3)]">
+            <h1 className="text-[3.375rem] font-medium">Conexe Conosco</h1>
           </div>
         </div>
       </div>
-      
-      <div className='bg-[#b6dcf7] h-3/5 w-full flex'>
-        <div className='bg-[#c6ecff] w-1/3'></div>
-        <div className='bg-[#d6fcff] w-1/3'></div>
-        <div className='bg-[#e6ffff] w-1/3'></div>
+
+      <div className="flex h-3/5 w-full bg-[#b6dcf7]">
+        <div className="w-1/3 bg-[#c6ecff]"></div>
+        <div className="w-1/3 bg-[#d6fcff]"></div>
+        <div className="w-1/3 bg-[#e6ffff]"></div>
       </div>
     </div>
   );
 };
+
 export default Footer;

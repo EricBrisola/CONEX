0ea0aa1ed1678ebc4a28f21235482947ebe570ff
EricBrisola <eric.coca13@gmail.com>
2024-06-14 15:33:43 -0300

    style: added a new font to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       boxShadow: {
-        'custom': '1px 6px 4.5px 0 rgba(0, 0, 0, 0.25)',
+        custom: "1px 6px 4.5px 0 rgba(0, 0, 0, 0.25)",
       },
       fontFamily: {
-        'JosefinSans': ['Josefin Sans', 'sans-serif']
-      }
+        JosefinSans: ["Josefin Sans", "sans-serif"],
+        ReenieBeanie: ["Reenie Beanie", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};

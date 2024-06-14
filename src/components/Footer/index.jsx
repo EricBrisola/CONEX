6320deee9f334d94e1dfa982aeaa7d763a7ae216
EricBrisola <eric.coca13@gmail.com>
2024-06-14 15:34:25 -0300

    fix: fixed broken x overflow

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import cursorImage from "../../assets/homePage/divConexe/cursormouse-1.png";
 
 const Footer = () => {
   return (
-    <div className="mt-[6.875rem] h-[29.375rem] w-screen">
+    <div className="max-w-screen mt-[6.875rem] h-[29.375rem]">
       <div className="flex h-2/5 w-full flex-col items-center justify-start">
         <div className="flex h-full w-full flex-col items-center justify-start overflow-visible rounded-t-full bg-[#b6dcf7]">
           <div className="relative top-[-6.25rem] flex h-[13.125rem] w-2/5 items-center justify-center rounded-full bg-[#b6dcf7] shadow-[-3.125rem_2.1875rem_0.3125rem_-0.9375rem_rgba(0,0,0,0.3)]">

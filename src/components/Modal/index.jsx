558cbf5891e19f8ad1057a7cdf6a706febaf4416
EricBrisola <eric.coca13@gmail.com>
2024-06-18 17:11:43 -0300

    feat: added modal to home page

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,4 +1,17 @@
-const Modal = () => {
-  return <div>Modal</div>;
+const Modal = ({ children, closeModal }) => {
+  return (
+    <section className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-black bg-opacity-40">
+      <article className="relative h-[27.813rem] w-[63rem] rounded-[2.875rem] bg-[#b6dcf7]">
+        <div className="relative">{children}</div>
+        <button
+          type="button"
+          className="absolute right-[2.375rem] top-[2.125rem] font-JosefinSans text-[3.125rem] leading-none"
+          onClick={closeModal}
+        >
+          X
+        </button>
+      </article>
+    </section>
+  );
 };
 export default Modal;

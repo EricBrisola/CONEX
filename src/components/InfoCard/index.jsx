b928c51b2687bbcc8cb6e067fe3e076a13491c88
EricBrisola <eric.coca13@gmail.com>
2024-06-18 17:12:15 -0300

    refactor: infoCard props were unstructured

--- src/components/InfoCard/index.jsx
+++ src/components/InfoCard/index.jsx
@@ -1,42 +1,33 @@
 /* eslint-disable react/prop-types */
-const InfoCard = ({
-  cardStyle,
-  title,
-  text,
-  mainImage,
-  background,
-  textBackground,
-  mainImageStyle,
-  backgroundStyle,
-  textBackgroundStyle,
-  textSectionStyle,
-  abreviatedTitle,
-  abreviatedTitleStyle,
-  btnStyle,
-  textStyle,
-}) => {
+const InfoCard = ({ infoCard }) => {
   return (
-    <article className={cardStyle}>
-      <img src={mainImage} alt="card-main-image" className={mainImageStyle} />
+    <article className={infoCard.cardStyle}>
       <img
-        src={background}
+        src={infoCard.mainImage}
+        alt="card-main-image"
+        className={infoCard.mainImageStyle}
+      />
+      <img
+        src={infoCard.background}
         alt="card-background-image"
-        className={backgroundStyle}
+        className={infoCard.backgroundStyle}
       />
 
-      {textBackground && (
+      {infoCard.textBackground && (
         <img
-          src={textBackground}
+          src={infoCard.textBackground}
           alt="text-background"
-          className={textBackgroundStyle}
+          className={infoCard.textBackgroundStyle}
         />
       )}
-      <p className={abreviatedTitleStyle}>{abreviatedTitle}</p>
-      <div className={textSectionStyle}>
-        <p className="text-xl font-bold">{title}</p>
-        <p className={textStyle}>{text}</p>
+      <p className={infoCard.abreviatedTitleStyle}>
+        {infoCard.abreviatedTitle}
+      </p>
+      <div className={infoCard.textSectionStyle}>
+        <p className="text-xl font-bold">{infoCard.title}</p>
+        <p className={infoCard.textStyle}>{infoCard.text}</p>
       </div>
-      <button className={btnStyle}>...</button>
+      <button className={infoCard.btnStyle}>...</button>
     </article>
   );
 };

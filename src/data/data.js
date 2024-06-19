d51937f223abb07e194ab85e6955aced6633bc66
EricBrisola <eric.coca13@gmail.com>
2024-06-19 19:51:15 -0300

    refactor: created a file to put all cards info

---
+++ src/data/data.js
@@ -0,0 +1,182 @@
+const data = [
+  {
+    id: 0,
+    cardStyle:
+      "relative flex h-[20.375rem] w-[52.5rem] justify-end font-JosefinSans",
+    title: "Psicologia e Saúde da Mulher",
+    text: "Aborda uma variedade de tópicos relacionados à saúde integral das mulheres: física, mental e emocional, em diferentes fases do desenvolvimento",
+    fullText:
+      "Aborda uma variedade de tópicos relacionados à saúde integral das mulheres: física, mental e emocional, em diferentes fases do desenvolvimento, desde a infância até a idade adulta e o envelhecimento. Busca proporcionar conhecimento, bem-estar físico, mental, psicológico e social.",
+    mainImage: "src/assets/homePage/Main/psm/psm-img.png",
+    background: "src/assets/homePage/Main/psm/psm-retangle.svg",
+    textBackground: "src/assets/homePage/Main/cloud-blue.png",
+    abbreviatedtitle: "PSM",
+    mainImageStyle: "absolute left-[0rem] top-[1.2rem] z-20 object-contain",
+    backgroundStyle: "absolute left-3 top-[0.825rem] z-10 object-contain",
+    textBackgroundStyle: "z-0 h-[18.625rem] w-[41.25rem]",
+    textSectionStyle:
+      "absolute right-[6rem] top-[5.21rem] flex w-[23.688rem] flex-col gap-[0.419rem]",
+    abbreviatedTitleStyle:
+      "absolute bottom-[0.3rem] left-24 h-[3.6rem] font-ReenieBeanie text-[4.063rem] leading-none",
+    btnStyle:
+      "absolute bottom-[4.95rem] right-[28.15rem] font-JosefinSans text-[3.125rem] leading-none",
+    textStyle: "text-wrap text-lg font-light leading-[1.51]",
+    modalStyles: {
+      mainContainer: "relative flex gap-7",
+      cardContainer:
+        "shadow-modalCustom relative h-[17.75rem] w-[24.5rem] rounded-bl-[4.75rem] rounded-br-[4.75rem] rounded-tl-[11.1875rem] rounded-tr-[4.75rem] bg-white",
+      imageStyle: "absolute left-[2.125rem] top-6",
+      textContainer:
+        "flex flex-col items-center justify-center gap-3 font-JosefinSans",
+      titleText: "text-2xl font-bold",
+      mainText: "w-[27.938rem] text-center text-xl font-light",
+      absoluteText:
+        "absolute bottom-0 right-2 text-center font-ReenieBeanie text-[4.0625rem] leading-none",
+    },
+  },
+  {
+    id: 1,
+    cardStyle:
+      "relative flex h-[23.513rem] w-[52.5rem] justify-end font-JosefinSans",
+    title: "Psicologia Infantil",
+    text: "Atua com crianças, ajudando-as a ter um desenvolvimento saudável. É um ramo da ciência que trata das questões psíquicas",
+    fullText:
+      "Atua com crianças, ajudando-as a ter um desenvolvimento saudável. É um ramo da ciência que trata das questões psíquicas de crianças. Assim, essa especialidade investiga e analisa o comportamento dessa faixa etária.",
+    mainImage: "src/assets/homePage/Main/pi/piImg.png",
+    background: "src/assets/homePage/Main/pi/piCircle.svg",
+    textBackground: "",
+    abbreviatedtitle: "PI",
+    mainImageStyle: "absolute right-4 top-[2.10rem] z-20 object-contain",
+    backgroundStyle: "absolute right-0 top-[1.7rem] z-10 object-contain",
+    textBackgroundStyle: "",
+    textSectionStyle:
+      "absolute right-[19rem] top-[8.5rem] flex w-[23.688rem] flex-col items-end gap-[0.419rem]",
+    abbreviatedTitleStyle:
+      "absolute right-[7.5rem] top-[20.163rem] h-[3.6rem] font-ReenieBeanie text-[4.063rem] leading-none",
+    btnStyle:
+      "absolute bottom-[6.3rem] left-[31.9rem] font-JosefinSans text-[3.125rem] leading-none",
+    textStyle: "text-wrap text-right text-lg font-light leading-[1.65]",
+    modalStyles: {
+      mainContainer:
+        "relative flex justify-center flex-row-reverse gap-[2.438rem]",
+      cardContainer:
+        "shadow-circleCustomModal relative h-[17.7375rem] w-[17.7375rem] rounded-full bg-white",
+      imageStyle: "absolute bottom-9 right-4",
+      textContainer:
+        "flex flex-col items-center justify-center gap-3 font-JosefinSans",
+      titleText: "text-2xl font-bold",
+      mainText: "w-[27.938rem] text-center text-xl font-light",
+      absoluteText:
+        "absolute -bottom-[1.5rem] right-[2.8rem] text-center font-ReenieBeanie text-[4.0625rem] leading-none",
+    },
+  },
+  {
+    id: 2,
+    cardStyle:
+      "relative flex h-[25.913rem] w-[52.5rem] justify-end font-JosefinSans",
+    title: "Psicologia Do Adulto E Do Idoso ",
+    text: "O atendimento psicológico para idosos é direcionado ao desenvolvimento cognitivo. Geralmente, essas terapias são desempenhadas",
+    fullText:
+      "Geralmente, essas terapias são desempenhadas em grupo para que o idoso também possa desenvolver a socialização. O psicólogo irá intermediar o contato do idoso com as situações desafiadoras da velhice, proporcionando um olhar diferente, e irá desenvolver atividades que ele possa executar e aprender ao mesmo tempo como encarar esses desafios.",
+    mainImage: "src/assets/homePage/Main/pai/father-img.png",
+    background: "src/assets/homePage/Main/pai/pai-circle.svg",
+    textBackground: "src/assets/homePage/Main/cloud-blue.png",
+    abbreviatedtitle: "PAI",
+    mainImageStyle: "absolute left-[3.4rem] z-20 object-contain",
+    backgroundStyle: "absolute left-5 top-16 z-10 object-contain",
+    textBackgroundStyle:
+      "absolute top-[4.75rem] z-0 h-[18.625rem] w-[41.25rem]",
+    textSectionStyle:
+      "absolute right-[6rem] top-[10.5rem] flex w-[23.688rem] flex-col gap-[0.419rem]",
+    abbreviatedTitleStyle:
+      "absolute bottom-0 left-32 h-[3.6rem] font-ReenieBeanie text-[4.063rem] leading-none",
+    btnStyle:
+      "absolute bottom-28 right-[28.16rem] font-JosefinSans text-[3.125rem] leading-none",
+    textStyle: "text-wrap text-lg font-light leading-[1.51]",
+    modalStyles: {
+      mainContainer: "relative flex w-[57rem] justify-center gap-[1.9rem]",
+      cardContainer:
+        "relative h-[17.563rem] w-[21.75rem] rounded-bl-[9.38rem] rounded-br-[6.25rem] rounded-tl-[12.5rem] rounded-tr-[12.5rem] bg-[#edf0eb] shadow-circleCustomModal",
+      imageStyle: "absolute -bottom-4 left-[3.3rem]",
+      textContainer:
+        "flex flex-col items-center justify-center font-JosefinSans",
+      titleText: "text-2xl font-bold",
+      mainText: "w-[30rem] text-center text-xl font-light",
+      absoluteText:
+        "absolute -bottom-3 -right-6 text-center font-ReenieBeanie text-[4.0625rem] leading-none",
+    },
+  },
+  {
+    id: 3,
+    cardStyle:
+      "relative flex h-[23.513rem] w-[52.5rem] justify-end font-JosefinSans",
+    title: "Arteterapia",
+    text: "Recorre a elementos da atividade artística como forma de intervenção profissional. É direcionada à qualidade de vida e melhora",
+    fullText:
+      "Recorre a elementos da atividade artística como forma de intervenção profissional. É direcionada à qualidade de vida e melhora da saúde. Abrange as mais diversas linguagens: pintura, dança, escultura, a história oral, música, modelagem, poesia, o audiovisual, entre outros. Uma forma de permitir o bem-estar e deixar fluir sentimentos por meio da expressão das obras.",
+    mainImage: "src/assets/homePage/Main/at/arImg 1.png",
+    background: "src/assets/homePage/Main/at/atRetangle.png",
+    textBackground: "",
+    abbreviatedtitle: "AT",
+    mainImageStyle: "absolute right-[2.6rem] top-[4.42rem] z-20 object-contain",
+    backgroundStyle: "absolute right-0 top-[1.7rem] z-10 object-contain",
+    textBackgroundStyle: "",
+    textSectionStyle:
+      "absolute right-[18.55rem] top-[6.7rem] flex w-[23.688rem] flex-col items-end gap-[0.419rem]",
+    abbreviatedTitleStyle:
+      "absolute right-[6rem] top-[17.4rem] h-[3.6rem] font-ReenieBeanie text-[4.063rem] leading-none",
+    btnStyle:
+      "absolute bottom-32 right-[18.55rem] font-JosefinSans text-[3.125rem] leading-none",
+    textStyle: "text-wrap text-right text-lg font-light leading-[1.65]",
+    modalStyles: {
+      mainContainer:
+        "relative flex justify-center flex-row-reverse gap-[2.438rem] pt-3",
+      cardContainer:
+        "relative h-[15.438rem] w-[18.625rem] rounded-bl-[5rem] rounded-br-[6.25rem] rounded-tl-[5rem] rounded-tr-[15.625rem] bg-white shadow-circleCustomModal",
+      imageStyle: "absolute bottom-2 right-14",
+      textContainer:
+        "flex flex-col items-center justify-center gap-1 font-JosefinSans",
+      titleText: "text-2xl font-bold",
+      mainText: "w-[27.938rem] text-center text-xl font-light",
+      absoluteText:
+        "absolute -bottom-[1.2rem] right-[3.4rem] text-center font-ReenieBeanie text-[4.0625rem] leading-none",
+    },
+  },
+  {
+    id: 4,
+    cardStyle:
+      "relative flex h-[23.513rem] w-[52.5rem] justify-end font-JosefinSans",
+    title: "Terapia Cognitivo Comportamental",
+    text: "Uma abordagem estruturada, estabelecida a partir da colaboração entre terapeuta e paciente. É recomendada e produz resultados",
+    fullText:
+      "Produz resultados eficazes para diversos transtornos psicológicos e emocionais, entre eles: Fobias; Ansiedade; Transtornos alimentares; Adicção; Depressão; Transtorno obsessivo-compulsivo (TOC); Comportamentais e psiquiátricos. A técnica consiste em buscar uma reestruturação cognitiva sobre emoções e comportamentos, por meio de intervenções sobre as cognições baseadas em eventos significativos para o paciente.",
+    mainImage: "src/assets/homePage/Main/tcc/tccImg.png",
+    background: "src/assets/homePage/Main/tcc/tccImg.svg",
+    textBackground: "src/assets/homePage/Main/cloud-blue.png",
+    abbreviatedtitle: "TCC",
+    mainImageStyle: "absolute left-[2.2rem] top-2 z-20 object-contain",
+    backgroundStyle: "absolute left-5 top-0 z-10 object-contain",
+    textBackgroundStyle: "absolute top-10 z-0 h-[18.625rem] w-[41.25rem]",
+    textSectionStyle:
+      "absolute right-[6rem] top-[8rem] flex w-[23.688rem] flex-col gap-[0.419rem]",
+    abbreviatedTitleStyle:
+      "absolute left-32 top-[20.163rem] h-[3.6rem] font-ReenieBeanie text-[4.063rem] leading-none",
+    btnStyle:
+      "absolute bottom-[7.1rem] left-[22.8rem] font-JosefinSans text-[3.125rem] leading-none",
+    textStyle: "text-wrap text-lg font-light leading-[1.51]",
+    modalStyles: {
+      mainContainer: "relative flex justify-center gap-[2.106rem]",
+      cardContainer:
+        "shadow-circleCustomModal relative h-[17.7375rem] w-[17.7375rem] rounded-full bg-[#edf0eb]",
+      imageStyle: "absolute bottom-4 left-0",
+      textContainer:
+        "flex flex-col items-center justify-center gap-2 font-JosefinSans",
+      titleText: "text-2xl font-bold",
+      mainText: "w-[34rem] text-center text-xl font-light",
+      absoluteText:
+        "absolute -bottom-[1rem] right-8 text-center font-ReenieBeanie text-[4.0625rem] leading-none",
+    },
+  },
+];
+
+export default data;

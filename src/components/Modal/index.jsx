2db7a52a755e3c1786018e08b8d3d0b01535681d
EricBrisola <eric.coca13@gmail.com>
2024-06-19 19:52:56 -0300

    chore: added a div to comport modal children

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -2,7 +2,11 @@ const Modal = ({ children, closeModal }) => {
   return (
     <section className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-black bg-opacity-40">
       <article className="relative h-[27.813rem] w-[63rem] rounded-[2.875rem] bg-[#b6dcf7]">
-        <div className="relative">{children}</div>
+        <div className="relative">
+          <div className="absolute left-[2.813rem] top-[4.5rem] h-[19.85rem] w-[54.25rem]">
+            {children}
+          </div>
+        </div>
         <button
           type="button"
           className="absolute right-[2.375rem] top-[2.125rem] font-JosefinSans text-[3.125rem] leading-none"

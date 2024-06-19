91e43711cbdf214a32b7839f2bd6d6f2baa72038
EricBrisola <eric.coca13@gmail.com>
2024-06-19 19:54:45 -0300

    feat: modal is now properly working and removed info cards to other file

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,128 +6,43 @@ import line3 from "../../assets/homePage/Main/line-3.svg";
 import line4 from "../../assets/homePage/Main/line-4.svg";
 import line5 from "../../assets/homePage/Main/line-5.svg";
 import InfoCard from "../../components/InfoCard";
+import data from "../../data/data";
 import Modal from "../../components/Modal";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  const infoCards = [
-    {
-      id: 0,
-      cardStyle:
-        "relative flex h-[20.375rem] w-[52.5rem] justify-end font-JosefinSans",
-      title: "Psicologia e saúde da mulher",
-      text: "Aborda uma variedade de tópicos relacionados à saúde integral das mulheres: física, mental e emocional, em diferentes fases do desenvolvimento",
-      fullText:
-        "Aborda uma variedade de tópicos relacionados à saúde integral das mulheres: física, mental e emocional, em diferentes fases do desenvolvimento, desde a infância até a idade adulta e o envelhecimento. Busca proporcionar conhecimento, bem-estar físico, mental, psicológico e social.",
-      mainImage: "src/assets/homePage/Main/psm/psm-img.png",
-      background: "src/assets/homePage/Main/psm/psm-retangle.svg",
-      textBackground: "src/assets/homePage/Main/cloud-blue.png",
-      abbreviatedtitle: "PSM",
-      mainImageStyle: "absolute left-[0rem] top-[1.2rem] z-20 object-contain",
-      backgroundStyle: "absolute left-3 top-[0.825rem] z-10 object-contain",
-      textBackgroundStyle: "z-0 h-[18.625rem] w-[41.25rem]",
-      textSectionStyle:
-        "absolute right-[6rem] top-[5.21rem] flex w-[23.688rem] flex-col gap-[0.419rem]",
-      abreviatedTitleStyle:
-        "absolute bottom-[0.3rem] left-24 h-[3.6rem] font-ReenieBeanie text-[4.063rem] leading-none",
-      btnStyle:
-        "absolute bottom-[4.95rem] right-[28.15rem] font-JosefinSans text-[3.125rem] leading-none",
-      textStyle: "text-wrap text-lg font-light leading-[1.51]",
-    },
-    {
-      id: 1,
-      cardStyle:
-        "relative flex h-[23.513rem] w-[52.5rem] justify-end font-JosefinSans",
-      title: "Psicologia Infantil",
-      text: "Atua com crianças, ajudando-as a ter um desenvolvimento saudável. É um ramo da ciência que trata das questões psíquicas",
-      fullText:
-        "Atua com crianças, ajudando-as a ter um desenvolvimento saudável. É um ramo da ciência que trata das questões psíquicas de crianças. Assim, essa especialidade investiga e analisa o comportamento dessa faixa etária.",
-      mainImage: "src/assets/homePage/Main/pi/piImg.png",
-      background: "src/assets/homePage/Main/pi/piCircle.svg",
-      textBackground: "",
-      abbreviatedtitle: "PI",
-      mainImageStyle: "absolute right-4 top-[2.10rem] z-20 object-contain",
-      backgroundStyle: "absolute right-0 top-[1.7rem] z-10 object-contain",
-      textBackgroundStyle: "",
-      textSectionStyle:
-        "absolute right-[19rem] top-[8.5rem] flex w-[23.688rem] flex-col items-end gap-[0.419rem]",
-      abreviatedTitleStyle:
-        "absolute right-[7.5rem] top-[20.163rem] h-[3.6rem] font-ReenieBeanie text-[4.063rem] leading-none",
-      btnStyle:
-        "absolute bottom-[6.3rem] left-[31.9rem] font-JosefinSans text-[3.125rem] leading-none",
-      textStyle: "text-wrap text-right text-lg font-light leading-[1.65]",
-    },
-    {
-      id: 2,
-      cardStyle:
-        "relative flex h-[25.913rem] w-[52.5rem] justify-end font-JosefinSans",
-      title: "Psicologia Do Adulto E Do Idoso ",
-      text: "O atendimento psicológico para idosos é direcionado ao desenvolvimento cognitivo. Geralmente, essas terapias são desempenhadas",
-      fullText:
-        "Geralmente, essas terapias são desempenhadas em grupo para que o idoso também possa desenvolver a socialização. O psicólogo irá intermediar o contato do idoso com as situações desafiadoras da velhice, proporcionando um olhar diferente, e irá desenvolver atividades que ele possa executar e aprender ao mesmo tempo como encarar esses desafios.",
-      mainImage: "src/assets/homePage/Main/pai/father-img.png",
-      background: "src/assets/homePage/Main/pai/pai-circle.svg",
-      textBackground: "src/assets/homePage/Main/cloud-blue.png",
-      abbreviatedtitle: "PAI",
-      mainImageStyle: "absolute left-[3.4rem] z-20 object-contain",
-      backgroundStyle: "absolute left-5 top-16 z-10 object-contain",
-      textBackgroundStyle:
-        "absolute top-[4.75rem] z-0 h-[18.625rem] w-[41.25rem]",
-      textSectionStyle:
-        "absolute right-[6rem] top-[10.5rem] flex w-[23.688rem] flex-col gap-[0.419rem]",
-      abreviatedTitleStyle:
-        "absolute bottom-0 left-32 h-[3.6rem] font-ReenieBeanie text-[4.063rem] leading-none",
-      btnStyle:
-        "absolute bottom-28 right-[28.16rem] font-JosefinSans text-[3.125rem] leading-none",
-      textStyle: "text-wrap text-lg font-light leading-[1.51]",
-    },
-    {
-      id: 3,
-      cardStyle:
-        "relative flex h-[23.513rem] w-[52.5rem] justify-end font-JosefinSans",
-      title: "Arteterapia",
-      text: "Recorre a elementos da atividade artística como forma de intervenção profissional. É direcionada à qualidade de vida e melhora",
-      fullText:
-        "Recorre a elementos da atividade artística como forma de intervenção profissional. É direcionada à qualidade de vida e melhora da saúde. Abrange as mais diversas linguagens: pintura, dança, escultura, a história oral, música, modelagem, poesia, o audiovisual, entre outros. Uma forma de permitir o bem-estar e deixar fluir sentimentos por meio da expressão das obras.",
-      mainImage: "src/assets/homePage/Main/at/arImg 1.png",
-      background: "src/assets/homePage/Main/at/atRetangle.png",
-      textBackground: "",
-      abbreviatedtitle: "AT",
-      mainImageStyle:
-        "absolute right-[2.6rem] top-[4.42rem] z-20 object-contain",
-      backgroundStyle: "absolute right-0 top-[1.7rem] z-10 object-contain",
-      textBackgroundStyle: "",
-      textSectionStyle:
-        "absolute right-[18.55rem] top-[6.7rem] flex w-[23.688rem] flex-col items-end gap-[0.419rem]",
-      abreviatedTitleStyle:
-        "absolute right-[6rem] top-[17.4rem] h-[3.6rem] font-ReenieBeanie text-[4.063rem] leading-none",
-      btnStyle:
-        "absolute bottom-32 right-[18.55rem] font-JosefinSans text-[3.125rem] leading-none",
-      textStyle: "text-wrap text-right text-lg font-light leading-[1.65]",
-    },
-    {
-      id: 4,
-      cardStyle:
-        "relative flex h-[23.513rem] w-[52.5rem] justify-end font-JosefinSans",
-      title: "Terapia Cognitivo Comportamental",
-      text: "Uma abordagem estruturada, estabelecida a partir da colaboração entre terapeuta e paciente. É recomendada e produz resultados",
-      fullText:
-        "Produz resultados eficazes para diversos transtornos psicológicos e emocionais, entre eles: Fobias; Ansiedade; Transtornos alimentares; Adicção; Depressão; Transtorno obsessivo-compulsivo (TOC); Comportamentais e psiquiátricos. A técnica consiste em buscar uma reestruturação cognitiva sobre emoções e comportamentos, por meio de intervenções sobre as cognições baseadas em eventos significativos para o paciente.",
-      mainImage: "src/assets/homePage/Main/tcc/tccImg.png",
-      background: "src/assets/homePage/Main/tcc/tccImg.svg",
-      textBackground: "src/assets/homePage/Main/cloud-blue.png",
-      abbreviatedtitle: "TCC",
-      mainImageStyle: "absolute left-[2.2rem] top-2 z-20 object-contain",
-      backgroundStyle: "absolute left-5 top-0 z-10 object-contain",
-      textBackgroundStyle: "absolute top-10 z-0 h-[18.625rem] w-[41.25rem]",
-      textSectionStyle:
-        "absolute right-[6rem] top-[8rem] flex w-[23.688rem] flex-col gap-[0.419rem]",
-      abreviatedTitleStyle:
-        "absolute left-32 top-[20.163rem] h-[3.6rem] font-ReenieBeanie text-[4.063rem] leading-none",
-      btnStyle:
-        "absolute bottom-[7.1rem] left-[22.8rem] font-JosefinSans text-[3.125rem] leading-none",
-      textStyle: "text-wrap text-lg font-light leading-[1.51]",
-    },
-  ];
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalCardInfo, setModalCardInfo] = useState(null);
+  const [infoCards, setInfoCards] = useState(null);
+
+  useEffect(() => {
+    setInfoCards(data);
+  }, []);
+
+  const getCurrentCardInfo = (currentCard) => {
+    setModalCardInfo({
+      title: currentCard.title,
+      abbreviatedtitle: currentCard.abbreviatedtitle,
+      fullText: currentCard.fullText,
+      mainImage: currentCard.mainImage,
+      mainContainer: currentCard.modalStyles.mainContainer,
+      cardContainer: currentCard.modalStyles.cardContainer,
+      imageStyle: currentCard.modalStyles.imageStyle,
+      textContainer: currentCard.modalStyles.textContainer,
+      titleText: currentCard.modalStyles.titleText,
+      mainText: currentCard.modalStyles.mainText,
+      absoluteText: currentCard.modalStyles.absoluteText,
+    });
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <main className="max-w-screen relative flex min-h-screen flex-col bg-[#EDF0EB]">
       <Header />
@@ -137,9 +52,18 @@ const Home = () => {
         </p>
       </section>
       <section className="relative mb-[18.344rem] flex flex-1 flex-col items-center">
-        {infoCards.map((infoCard) => {
-          return <InfoCard key={infoCard.id} infoCard={infoCard} />;
-        })}
+        {infoCards &&
+          infoCards.map((infoCard) => {
+            return (
+              <InfoCard
+                key={infoCard.id}
+                infoCard={infoCard}
+                onClick={() => {
+                  openModal(), getCurrentCardInfo(infoCard);
+                }}
+              />
+            );
+          })}
         <img src={line1} alt="line-1" className="absolute top-[15.8rem] z-30" />
         <img
           src={line2}
@@ -162,14 +86,26 @@ const Home = () => {
         alt="line-5"
         className="absolute bottom-[13rem] left-1/2 z-0 -translate-x-1/2 -translate-y-1/2 transform"
       />
-      <Modal>
-        <article className="absolute left-[2.813rem] top-[4.5rem] h-[19.85rem] w-[54.25rem] bg-slate-500">
-          <div className="">
-            <p className="text-xl font-bold"></p>
-            <p className="">{infoCards[0].fullText}</p>
+      {isModalOpen && (
+        <Modal closeModal={closeModal}>
+          <div className={modalCardInfo.mainContainer}>
+            <div className={modalCardInfo.cardContainer}>
+              <img
+                src={modalCardInfo.mainImage}
+                alt="main-image"
+                className={modalCardInfo.imageStyle}
+              />
+            </div>
+            <div className={modalCardInfo.textContainer}>
+              <p className={modalCardInfo.titleText}>{modalCardInfo.title}</p>
+              <p className={modalCardInfo.mainText}>{modalCardInfo.fullText}</p>
+            </div>
           </div>
-        </article>
-      </Modal>
+          <p className={modalCardInfo.absoluteText}>
+            {modalCardInfo.abbreviatedtitle}
+          </p>
+        </Modal>
+      )}
       <Footer />
     </main>
   );

c08f01c5dfdc12adb4ca793549c4a6409613d6ba
EricBrisola <eric.coca13@gmail.com>
2024-06-21 00:10:12 -0300

    feat: added ProfessionalSection to professionals page

--- src/pages/Professionals/index.jsx
+++ src/pages/Professionals/index.jsx
@@ -1,9 +1,16 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import ProfessionalCard from "../../components/ProfessionalCard";
-import ProfessionalCardContent from "../../components/ProfessionalCardContent";
+import data from "../../data/professionalsData";
+import ProfessionalSection from "../../components/ProfessionalSection";
+import { useState, useEffect } from "react";
 
 const Professionals = () => {
+  const [professionalsInfos, setProfessionalsInfos] = useState(null);
+
+  useEffect(() => {
+    setProfessionalsInfos(data);
+  }, []);
+
   return (
     <main className="max-w-screen relative flex min-h-screen flex-col bg-[#EDF0EB]">
       <Header />
@@ -12,17 +19,28 @@ const Professionals = () => {
           CONHEÇA AS ÁREAS ATENDIDAS
         </p>
       </section>
-      <section className="relative mb-[18.344rem] flex flex-1 flex-col items-center">
-        <article className="relative flex h-[39.5rem] w-[68.875rem] items-center">
-          <div className="absolute left-0 top-0 z-20 h-[30.438rem] w-[65.438rem] rounded-[2.188rem] bg-[#b6dcf7]">
-            <p className="pt-3 text-center font-JosefinSans text-[2rem] font-light">
-              Psicologia e Saúde da Mulher
-            </p>
-          </div>
-          <ProfessionalCard />
-          <ProfessionalCardContent />
-          <div className="absolute bottom-0 right-0 z-10 h-[30.438rem] w-[65.438rem] rounded-[2.188rem] border-[0.4375rem] border-[#b6dcf7]"></div>
-        </article>
+      <section className="relative mb-[18.344rem] flex flex-1 flex-col items-center gap-40">
+        {professionalsInfos &&
+          professionalsInfos.map((professional) => {
+            return (
+              <ProfessionalSection
+                key={professional.id}
+                blueCardStyle={professional.blueCardStyle}
+                blueBorderStyle={professional.blueBorderStyle}
+                title={professional.title}
+                titleStyle={professional.titleStyle}
+                picture={professional.image}
+                name={professional.name}
+                crp={professional.crp}
+                phone={professional.phoneNumber}
+                email={professional.email}
+                infoStyle={professional.infoCardStyle}
+                image={professional.secondaryImage}
+                text={professional.text}
+                contentStyle={professional.textSectionStyle}
+              />
+            );
+          })}
       </section>
       <Footer />
     </main>
